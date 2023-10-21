f0f14fd6fdea9c3f96c502346f38e94586c6abac
riojr100 <riojr100@gmail.com>
2023-10-21 16:16:26 +0700

    yes

--- login.php
+++ login.php
@@ -5,8 +5,6 @@ if (isset($_SESSION['user_id'])) {
     exit();
 }
 
-include('header.php');
-
 if (isset($_POST['login'])) {
     include('login_handler.php');
 }
@@ -17,21 +15,39 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>Document</title>
 </head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<body>
-    <div id="login-form">
-        <h2>Login</h2>
-        <form action="login.php" method="post">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" name="login" value="Login">
-        </form>
+<body class="body-login">
+    <div class="form-bg">
+        <div class="container">
+            <h1 class="app-title">Task Tracker</h1>
+            <div class="row">
+                <div class="col-md-offset-3 col-md-6">
+                    <form class="form-horizontal" action="login.php" method="post">
+                        <span class="heading">Log In</span>
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control" id="inputUsername" placeholder="Username">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <div class="form-group help">
+                            <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password">
+                            <i class="fa fa-lock"></i>
+                        </div>
+                        <div class="form-group">
+                            <p>Don't have an account? <a href="register.php">Register here!</a></p>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-default">Log In</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-
 </body>
 
 </html>
