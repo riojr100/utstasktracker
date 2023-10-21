0b4c60beaccbe6add843d0ceb3779f31ce12d45c
riojr100 <riojr100@gmail.com>
2023-10-21 15:51:22 +0700

    yes

--- login.php
+++ login.php
@@ -9,9 +9,18 @@ if (isset($_POST['login'])) {
     include('login_handler.php');
 }
 ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 
 <body class="body-login">
     <div class="form-bg">
@@ -42,6 +51,5 @@ if (isset($_POST['login'])) {
         </div>
     </div>
 </body>
-<?php
-include('footer.php');
-?>
+
+</html>
