b02043f367fef31c3900d87145ebcb8f7bc6977d
Reinhard Javera Maheswara <71541330+reinhardjavera@users.noreply.github.com>
2023-10-21 16:07:54 +0700

    yes

--- login.php
+++ login.php
@@ -9,17 +9,9 @@ if (isset($_POST['login'])) {
     include('login_handler.php');
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <body class="body-login">
     <div class="form-bg">
@@ -49,4 +41,6 @@ if (isset($_POST['login'])) {
         </div>
     </div>
 </body>
-</html>
+<?php
+include('footer.php');
+?>
