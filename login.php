e39b1677e48c2a4d33136adb5564c3e4ca770eb2
Reinhard Javera Maheswara <71541330+reinhardjavera@users.noreply.github.com>
2023-10-21 21:48:40 +0700

    Register and Login Done

--- login.php
+++ login.php
@@ -15,18 +15,38 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<body class="body-login">
+<body class="body-login-register">
     <div class="form-bg">
         <div class="container">
             <h1 class="app-title">Task Tracker</h1>
-            <div class="row">
+            <div class="row justify-content-center">
+
+                <?php
+                if (isset($_POST['loginFailed'])) {
+                ?>
+                    <div class="p-3 mb-2 bg-warning text-dark rounded">
+                        Login failed. Username not found or password is incorrect.
+                    </div>
+                <?php
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['registerSuccess'])) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        Your Registration Was Successful!
+                    </div>
+                <?php
+                }
+                ?>
+
                 <div class="col-md-offset-3 col-md-6">
                     <form class="form-horizontal" action="login.php" method="post">
                         <span class="heading">Log In</span>
--- register.php
+++ register.php
@@ -4,19 +4,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 
-<body>
-    <div id="login-form">
-        <h2>Register</h2>
-        <form action="register_handler.php" method="post">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="password" name="passwordConfirm" placeholder="Confirm Password" required>
-            <input type="submit" name="register" value="Register">
-            <p>Don't have an account? <a href="register.php">Register here!</a></p>
-        </form>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body class="body-login-register">
+    <div class="form-bg">
+        <div class="container">
+            <h1 class="app-title">Task Tracker</h1>
+            <div class="row justify-content-center">
+
+                <?php
+                if (isset($_POST['unmatched'])) { ?>
+                    <div class="alert alert-danger" role="alert">
+                        Password and Confirm Password do not match
+                    </div>
+                <?php
+                }
+                ?>
+
+                <div class="col-md-offset-3 col-md-6">
+                    <form class="form-horizontal" action="register.php" method="post">
+                        <span class="heading">Registration</span>
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control" id="form-control" placeholder="Username">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control" id="form-control" placeholder="Password">
+                            <i class="fa fa-lock"></i>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="passwordConfirm" class="form-control" id="" placeholder="Confirm Password">
+                            <i class="fa fa-key"></i>
+                        </div>
+                        <div class="form-group">
+                            <p>Already have an account? <a href="login.php">Log In here!</a></p>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="register" value="register" class="btn btn-default" id="btn">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
