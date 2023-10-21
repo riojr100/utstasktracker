725e6f99ed4c09c3b95f7215355cac9ae16e4e9e
Reinhard Javera Maheswara <71541330+reinhardjavera@users.noreply.github.com>
2023-10-21 15:35:11 +0700

    Login

--- login.php
+++ login.php
@@ -5,20 +5,43 @@ if (isset($_SESSION['user_id'])) {
     exit();
 }
 
-include('header.php');
-
 if (isset($_POST['login'])) {
     include('login_handler.php');
 }
 ?>
-<div id="login-form">
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <input type="submit" name="login" value="Login">
-    </form>
-</div>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body class="body-login">
+    <div class="form-bg">
+        <div class="container">
+            <h1 class="app-title">Task Tracker</h1>
+
+            <div class="row">
+                <div class="col-md-offset-3 col-md-6">
+                    <form class="form-horizontal" action="login.php" method="post">
+                        <span class="heading">Log In</span>
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control" id="inputUsername" placeholder="Username">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <div class="form-group help">
+                            <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password">
+                            <i class="fa fa-lock"></i>
+                        </div>
+                        <div class="form-group">
+                            <p>Don't have an account? <a href="register.php">Register here!</a></p>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-default">Log In</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 <?php
 include('footer.php');
 ?>
