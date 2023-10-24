80b6b85d524d994d4284824b4a794d9ff3cf9dce
riojr100 <riojr100@gmail.com>
2023-10-24 22:03:30 +0700

    update pass db

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "BM-;al1xn-=e";
 $dbname = "todo_list";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
